777e73b3948cd1cb0e103aafa4d3aafc4cd9d2fc
Joshua Greaves <joshua.greaves@googlemail.com>
2016-12-08 23:53:05 -0700

    Lots of cleanup, added readme and gitignore

---
+++ README.md
@@ -0,0 +1,2 @@
+# Utils
+This is a simple utilities file designed to speed up the "plumbing" for data scientists and anyone working with images. It will be expanded over time to fit other domains with useful repeatable functions
--- utils.py
+++ utils.py
@@ -3,16 +3,15 @@ import numpy as np
 import os
 
 
-def batch(Data, Labels, BatchSize = 32):
+def batch(data, batch_size=32):
     """Create a new batch of data
+    Uses the size of the first element in the list to get the max index
     
     Parameters
     ----------
-    Data : numpy.ndarray
-        The data to batch
-    Labels : numpy.ndarray
-        The labels to batch
-    BatchSize : int
+    data : list(numpy.ndarray) or numpy.ndarray
+        A list of the data to batch, or one array of data
+    batch_size : int
         The size of the batch to generate
 
     Returns
@@ -20,42 +19,87 @@ def batch(Data, Labels, BatchSize = 32):
     numpy.ndarray, numpy.ndarray
         An subset of data, and a subset of labels
     """
+    results = None
+    if type(data) == list:
+        indices = np.random.choice(len(data[0]), batch_size, False)
+        results = []
+        for x in data:
+            results.append(x[indices])
 
-    # Generate some random indices
-    indices = np.random.choice(len(Data), BatchSize, False)
-    return Data[indices], Labels[indices]
+    elif type(data) == np.ndarray:
+        indices = np.random.choice(len(data), batch_size, False)
+        results = data[indices]
+
+    else:
+        raise RuntimeError("Invalid type for data, must be a list or numpy array")
+    return results
 
 
 
 
-def loadImage(Path, Color=True):
+def load_image(path, color=True, resize=False,
+              height=100, width=100, maintain_aspect_ratio=True,
+              crop=False, padding="White"):
     """Loads a single image
+    If resize and crop, it will be cropped
+    If resize and not crop and maintain_aspect_ratio it will be padded
+    If resize and not maintain_aspect_ration it will be squashed/stretched
 
     Parameters
     ----------
-    Path : str
-        The full or relative path to the image
-    Color : bool
-        Whether the image should be loaded in color (True) or grayscale (False)
+    path : str
+        The full or relative path of the image
+    color : bool
+        True for loading the image in color, or False for grayscale
+    resize : bool
+        True to resize the images when loading them
+    height : int
+        The height to load the picture at (only if resize == True)
+    width : int
+        The width to load the picture at (only if resize == True)
+    maintain_aspect_ratio : bool
+        True to maintain aspect ratio (will be padded) (only if resize == True)
+    crop : bool
+        If True, it will fit the shortest side and crop the rest (only if resize == True)
+    padding : str
+        "White" or "Black"
     
     Returns
     -------
     numpy.ndarray
         The image in a numpy array
     """
+    img = cv2.imread(path, color)
+    
+    if resize:
+        if maintain_aspect_ratio:
+            if crop:
+                if img.shape[0] / float(height) < img.shape[1] / float(height):
+                    img = resize_image(img, height=height)
+                else:
+                    img = resize_image(img, width=width)
+            else:
+                if img.shape[0] / float(height) < img.shape[1] / float(width):
+                    img = resize_image(img, width=width)
+                else:
+                    img = resize_image(img, height=height)
+        else:
+            img = resize_image(img, height=height, width=width)
+        
+        img = crop_or_pad(img, height, width, padding)
 
-    return cv2.imread(Path, Color)
+    return img
 
 
 
-def saveImage(Img, Path):
+def save_image(img, path):
     """Saves a single image
 
     Parameters
     ----------
-    Img : numpy.ndarray
+    img : numpy.ndarray
         The image to save
-    Path : str
+    path : str
         The path and name to save the image to
     
     Returns
@@ -63,167 +107,78 @@ def saveImage(Img, Path):
     None
 
     """
+    cv2.imwrite(path, img)
 
-    cv2.imwrite(Path, Img)
 
-
-def loadImageNames(Path):
+def load_image_names(path):
     """Loads the name of all images in a directory
     Will look for all .jpeg, .jpg, .png, and .tiff files
 
     Parameters
     ----------
-    Path : str
+    path : str
         The full or relative path of the directory
 
     Returns
     -------
-    str, list(str)
-        The path, and a list of all the file names
+    list(str)
+        A sorted list of all the file names
 
     """
-    return sorted( img for img in os.listdir(Path) if (os.path.isfile(os.path.join(Path, img))
+    return sorted( img for img in os.listdir(path) if (os.path.isfile(os.path.join(path, img))
                                                     and img.lower().endswith(('.jpg', '.jpeg', '.png', '.tiff'))))
 
 
-def loadImagesInDirectory(Path, Color=True, Padding="White",
-                          ResizeImages=False, Width=100, Height=100, MaintainAspectRatio=True,
-                          Crop=False):
+def load_images_in_directory(path, color=True,
+                             height=100, width=100, maintain_aspect_ratio=True,
+                             crop=False, padding="White"):
     """Loads all images in a directory
     Will look for all .jpeg, .jpg, .png, and .tiff files
     
     Parameters
     ----------
-    Path : str
-        The full or relative path of the directory
-    Color : bool
+    path : str
+        The full or relative path of the image
+    color : bool
         True for loading the image in color, or False for grayscale
-    Padding : str
+    height : int
+        The height to load the picture at
+    width : int
+        The width to load the picture at
+    maintain_aspect_ratio : bool
+        True to maintain aspect ratio (will be padded)
+    crop : bool
+        If True, it will fit the shortest side and crop the rest
+    padding : str
         "White" or "Black"
-    ResizeImages : bool
-        True to resize the images when loading them
-    Width : int
-        The width to load the picture at (only if ResizeImages == True)
-    Height : int
-        The height to load the picture at (only if ResizeImages == True)
-    MaintainAspectRatio : bool
-        True to maintain aspect ratio (will be padded) (only if ResizeImages == True)
-    Crop : bool
-        If True, it will fit the shortest side and crop the rest (only if ResizeImages == True)
     
     Returns
     -------
     numpy.ndarray
-        An array of all the images of shape [num_images, width, height, channels]
+        An array of all the images of shape [num_images, height, width, channels]
     """
 
-    # Helper functions
-    # Handles the different options for resizing images
-    def resizeEachImage():
-        # If we are to maintain the aspect ratio
-        if MaintainAspectRatio == True:
-
-            # If it is cropped we need the shortest edge
-            if Crop:
-                if loadedImage.shape[0] / float(Height) < loadedImage.shape[1] / float(Width):
-                    # Height is further from edge than width
-                    resizedImage = resizeImage(loadedImage, Height = Height)
-                else:
-                    # Width is further from edge than height
-                    resizedImage = resizeImage(loadedImage, Width = Width)
-            else:
-                if loadedImage.shape[0] / float(Height) < loadedImage.shape[1] / float(Width):
-                    resizedImage = resizeImage(loadedImage, Width = Width)
-                else:
-                    resizedImage = resizeImage(loadedImage, Height = Height)
-        
-        # If not MaintainAspectRatio
-        else:
-            resizedImage = resizeImage(loadedImage, Height, Width)
-        
-        #print loadedImage.shape
-        #print resizedImage.shape
-        return resizedImage
+    img_names = load_image_names(path)
+    result = np.empty([len(img_names), height, width, 3 if color else 1], dtype=np.uint8)
+    for i, x in enumerate(img_names):
+        result[i] = load_image(os.path.join(path, x), color=color,
+                               resize=True, height=height, width=width,
+                               maintain_aspect_ratio=maintain_aspect_ratio, crop=crop,
+                               padding=padding)
+    return result
 
 
 
-    # Some important variables
-    images = []
-    imageShapes = []
-    maxWidth = 0
-    maxHeight = 0
-
-    if Color == True:
-        channels = 3
-    else:
-        channels = 1
-
-    # Get all images in directory
-    for img in sorted( img for img in os.listdir(Path) if (os.path.isfile(os.path.join(Path, img))
-                                                    and img.lower().endswith(('.jpg', '.jpeg', '.png', '.tiff')))):
-            # Load the image
-            loadedImage = loadImage(os.path.join(Path, img), Color=Color)
-
-            # If we need to resize the images
-            if ResizeImages:
-                resizedImage = resizeEachImage()
-                #print resizedImage.shape
-                finalImage = cropOrPad(resizedImage, Height, Width, Padding=Padding)
-                #print finalImage.shape
-                images.append(finalImage)
-            
-            # Not resizing
-            else:         
-                # Collect max width and height
-                if loadedImage.shape[1] > maxWidth: maxWidth = loadedImage.shape[1]
-                if loadedImage.shape[0] > maxHeight: maxHeight = loadedImage.shape[0]
-    
-                # Save the image and it's shape
-                images.append(loadedImage)
-                imageShapes.append(loadedImage.shape)
-    
-    # If Resize images, set the max height and width
-    if ResizeImages:
-        maxHeight = Height
-        maxWidth = Width
-
-    # Make a numpy array to hold all the images
-    # If it is color:
-    if Color:
-        imageArray = np.empty((len(images), maxHeight, maxWidth, channels), dtype=np.uint8)
-    else:
-        imageArray = np.empty((len(images), maxHeight, maxWidth), dtype=np.uint8)
-
-
-    if ResizeImages:
-        # Loop through every image and add it to the numpy array
-        for i, img in enumerate(images):
-            imageArray[i] = img
-        
-    else:
-        # Loop through every image and pad it
-        for i, img in enumerate(images):
-            img2 = cropOrPad(img, maxHeight, maxWidth, Padding=Padding)
-    
-            # Insert the image
-            imageArray[i] = img2
-        
-    return imageArray
-        
-
-
-
-
-def resizeImage(Img, Height = 0, Width = 0):
+def resize_image(img, height=0, width=0):
     """Resize an image to a desired height and width
     
     Parameters
     ----------
-    Img : numpy.ndarray
+    img : numpy.ndarray
         The image to resize
-    Height : int
+    height : int
         The max height you want the image to be. If 0, it is calculated from Width
-    Width : int
+    width : int
         The max width you want the image to be. If 0, it is calculated from Height
     
     Returns
@@ -233,44 +188,44 @@ def resizeImage(Img, Height = 0, Width = 0):
     """
 
     # If both are zero, we don't know what to resize it to!
-    if (Height == 0 and Width == 0):
+    if (height == 0 and width == 0):
         raise ValueError("Height and Width can't both be 0!")
-    elif (Height < 0 or Width < 0):
+    elif (height < 0 or width < 0):
         raise ValueError("Height or Width can't be below 0")
-    elif (Height == 0):
+    elif (height == 0):
         # We need to caluclate the scale from the width
-        scale = float(Width) / Img.shape[1]
-    elif (Width == 0):
+        scale = float(width) / img.shape[1]
+    elif (width == 0):
         # we need to calculate the scale from the height
-        scale = float(Height) / Img.shape[0]
+        scale = float(height) / img.shape[0]
     else:
         # In this case, the image will not maintain aspect ratio
-        if Img.shape[0] > Height:
-            return cv2.resize(Img, (Width, Height), interpolation=cv2.INTER_AREA)
+        if img.shape[0] > height:
+            return cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
         else:
-            return cv2.resize(Img, (Width, Height), interpolation=cv2.INTER_CUBIC)
+            return cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
     
     # If the scale factor is larger:
     if scale > 1:
-        return cv2.resize(Img, None, fx = scale, fy = scale, interpolation=cv2.INTER_CUBIC)
+        return cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
     else:
-        return cv2.resize(Img, None, fx = scale, fy = scale, interpolation=cv2.INTER_AREA)
+        return cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
     
 
 
 
-def cropOrPad(Img, Height, Width, Padding="White"):
+def crop_or_pad(img, height, width, padding="White"):
     """Puts the image into a new array of Height x Width, and crops or pads as necessary
 
     Parameters
     ----------
-    Img : numpy.ndarray
+    img : numpy.ndarray
         The image to put onto the canvas
-    Height : int
+    height : int
         The desired height of the returned image
-    Width : int
+    width : int
         The desired width of the returned image
-    Padding : str
+    padding : str
         The color to pad with. "White" or "Black" 
     
     Returns
@@ -279,86 +234,70 @@ def cropOrPad(Img, Height, Width, Padding="White"):
         The image after having been cropped or padded
     """
 
-    startY = int(round((Height - Img.shape[0]) / 2.0))
-    startX = int(round((Width - Img.shape[1]) / 2.0))
-    endX = 0
-    endY = 0
+    start_y = int(round((height - img.shape[0]) / 2.0))
+    start_x = int(round((width - img.shape[1]) / 2.0))
+    end_x = 0
+    end_y = 0
 
     # If these are less than 0, we must trim some
-    imageStartY = 0
-    imageStartX = 0
-    imageEndY = Img.shape[0]
-    imageEndX = Img.shape[1]
+    img_start_y = 0
+    img_start_x = 0
+    img_end_y = img.shape[0]
+    img_end_x = img.shape[1]
     
-    if startY < 0:
-        imageStartY -= startY
-        imageEndY = imageStartY + Height
-        startY = 0
-        endY = Height
+    if start_y < 0:
+        img_start_y -= start_y
+        img_end_y = img_start_y + height
+        start_y = 0
+        end_y = height
     else:
-        endY = startY + Img.shape[0]
+        end_y = start_y + img.shape[0]
 
-    if startX < 0:
-        imageStartX -= startX
-        imageEndX = imageStartX + Width
-        startX = 0
-        endX = Width
+    if start_x < 0:
+        img_start_x -= start_x
+        img_end_x = img_start_x + width
+        start_x = 0
+        end_x = width
     else:
-        endX = startX + Img.shape[1]
-    
-    # print "Height: " + str(Height) + ", Width: " + str(Width)
-    # print "startY: " + str(startY) + ", startX: " + str(startX)
-    # print "endY: " + str(endY) + ", endX: " + str(endX)
-    # print "imageStartY: " + str(imageStartY) + ", imageStartX: " + str(imageStartX)
-    # print "imageEndY: " + str(imageEndY) + ", imageEndX: " + str(imageEndX)
-    # print "Image shape: ", Img.shape
-    # exit()
+        end_x = start_x + img.shape[1]
 
     # If it is a full color image
-    if len(Img.shape) == 3:
-        if Padding == "White":
-            array = np.full((Height, Width, 3), 255, dtype=np.uint8)
-        elif Padding == "Black":
-            array = np.zeros((Height, Width, 3), dtype=np.uint8)
+    if img.shape[2] == 3:
+        if padding == "White":
+            array = np.full((height, width, 3), 255, dtype=np.uint8)
+        elif padding == "Black":
+            array = np.zeros((height, width, 3), dtype=np.uint8)
         else:
-            raise ValueError("Unknown parameter for Padding, " + Padding + ". Must be Black or White")
+            raise ValueError("Unknown parameter for pading, " + padding + ". Must be Black or White")
     else:
-        if Padding == "White":
-            array = np.full((Height, Width), 255, dtype=np.uint8)
-        elif Padding == "Black":
-            array = np.zeros((Height, Width), dtype=np.uint8)
+        if padding == "White":
+            array = np.full((height, width, 1), 255, dtype=np.uint8)
+        elif padding == "Black":
+            array = np.zeros((height, width, 1), dtype=np.uint8)
         else:
-            raise ValueError("Unknown parameter for Padding, " + Padding + ". Must be Black or White")
+            raise ValueError("Unknown parameter for padding, " + padding + ". Must be Black or White")
     
     # Insert the image into the array
-    array[startY:endY, startX:endX] = Img[imageStartY:imageEndY, imageStartX:imageEndX]
+    array[start_y:end_y, start_x:end_x] = img[img_start_y:img_end_y, img_start_x:img_end_x]
     return array
 
 
 
 
-def displayImage(Img, Name="Image", Batch=False):
-    """Displays a single image from an array
+def display_images(imgs):
+    """Displays all images in a batch
     
     Parameters
     ----------
     img : numpy.ndarray
-        The image in a numpy array
-    name : str
-        The name of the window
-    Img2 : numpy.ndarray
-        A second image to display. If None, will just display one image
+        The image in a numpy array [batch, height, width, channels]
     
     Returns
     -------
     None
     """
 
-    if Batch is True:
-        for i in Img:
-            cv2.imshow(Name, i)
-    else:
-        cv2.imshow(Name, Img)
-    cv2.waitKey(0)
+    for i, x in enumerate(img):
+        cv2.imshow("img" + str(i), x)
+    cv2.waitKey()
     cv2.destroyAllWindows()
-    return
